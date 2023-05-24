c136d3d2cd880fa59b853d68fa06b4a8dcd11f94
Victor Cracel <vcmessner@hotmail.com.br>
2023-05-24 05:23:33 -0300

    New cucumber struts project

---
+++ Cucumber_Exercise/hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,63 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+import org.junit.jupiter.api.Assertions.*;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    //@Given("today is Sunday")
+    //public void today_is_Sunday() {
+   //     today = "Sunday";
+   // }
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Given("today is Friday")
+    public void today_is_Friday() {
+    today = "Friday";
+    }
+
+
+
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
---
+++ Cucumber_Exercise/simple_struts_exercise/src/main/webapp/success.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Success!</title>
+</head>
+<body>
+<h2>Thank you for giving us your name!</h2>
+<p>
+Your name is: <h3><s:property value="name" /></h3>
+</p>
+</body>
+</html>
